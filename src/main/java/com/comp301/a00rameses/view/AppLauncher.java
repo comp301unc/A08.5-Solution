1b9acaec9d8d98d307d6d6615b3b929ff371e6e1
Edward Baker <eddean@email.unc.edu>
2022-12-01 16:00:43 -0500

    copy for students

--- src/main/java/com/comp301/a00rameses/view/AppLauncher.java
+++ src/main/java/com/comp301/a00rameses/view/AppLauncher.java
@@ -1,9 +1,7 @@
 package com.comp301.a00rameses.view;
 
 import com.comp301.a00rameses.controller.ClassicMvcController;
-import com.comp301.a00rameses.controller.ControllerImpl;
 import com.comp301.a00rameses.model.Model;
-import com.comp301.a00rameses.model.ModelImpl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,12 +9,5 @@ public class AppLauncher extends Application {
   @Override
   public void start(Stage stage) {
     // TODO: Create your Model, View, and Controller instances and launch your GUI
-    Model model = new ModelImpl();
-    ClassicMvcController controller = new ControllerImpl(model);
-    MainView view = new MainView(model, controller);
-
-    stage.setScene(view.getScene());
-    stage.setTitle("Does Rameses Love Me?");
-    stage.show();
   }
 }
