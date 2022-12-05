2c50429e96137fde621d195727814eb3cea68cd8
Edward Baker <eddean@email.unc.edu>
2022-12-05 15:31:42 -0500

    added alternate mvc interface

--- src/main/java/com/comp301/a00rameses/controller/ClassicMvcController.java
+++ src/main/java/com/comp301/a00rameses/controller/ClassicMvcController.java
@@ -2,5 +2,5 @@ package com.comp301.a00rameses.controller;
 
 public interface ClassicMvcController {
   /** Handles the click action to go ask loves me or loves me not */
-  void askbutton();
+  void askButton();
 }
